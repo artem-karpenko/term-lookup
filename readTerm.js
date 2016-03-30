d9cf4d42c95a4e2e473a463b17210976fdb6c6da
Artem Karpenko <gooyozh@gmail.com>
2016-03-30 21:59:44 +0300

    Read from file at offset

---
+++ readTerm.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var file = process.argv[2];
+var offset = process.argv[3];
+var length = process.argv[4] || 50;
+
+fs.open(file, 'r', (err, fd) => {
+    var buffer;
+    fs.read(fd, buffer, 0, length, offset, (err, bytesRead, buffer) => {
+        console.log(buffer.toString());
+        fs.close(fd);
+    });
+});
